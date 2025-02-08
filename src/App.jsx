82bda0daaf6d6e698f04cad574243c81fc470ac7
John Rogee Turqueza <jrogeet@gmail.com>
2025-02-08 23:48:42 +0800

    - Added a sort selection

--- src/App.jsx
+++ src/App.jsx
@@ -81,14 +81,45 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  // sort by the default value:
+  if (sortBy === "input") sortedItems = items;
+
+  // sort by description alphabetically:
+  if (sortBy === "description") sortedItems = items.slice().sort((a,b) => 
+    a.description.localeCompare(b.description));
+
+  // slice() before to copy and not modify the
+
+  // sort by packed status:
+  if (sortBy === "packed") sortedItems = items
+    .slice()
+    .sort((a,b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
         {/* Iterate through the Items */}
-        {items.map(item=>
-          <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />
-        )}
+        {sortedItems.map(item=> (
+          <Item 
+            item={item} 
+            onDeleteItem={onDeleteItem} 
+            onToggleItem={onToggleItem} 
+            key={item.id} 
+          />
+        ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
